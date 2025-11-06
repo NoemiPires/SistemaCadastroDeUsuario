3a5e1a002ea9ba88e345e96a56d8602945d22995
guoliveira0 <luizgustavo160206@gmail.com>
2025-11-06 10:03:49 -0300

    Commits for class Item, produto and Vendedor

---
+++ Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroDeUsuario
+{
+    public class Item
+    {
+        public UInt32 Id { get; set; }
+        public UInt32 Quantidade { get; set; }
+        public Decimal PrecoUnitario { get; set; }
+        public Decimal Desconto { get; set; }
+
+        public Decimal CalcularTotal()
+        {
+            return PrecoUnitario * Quantidade - (PrecoUnitario * Desconto);
+        }
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroDeUsuario
+{
+    public class Produto
+    {
+        public String Nome { get; set; }
+        public Decimal Preco { get; set; }
+        public UInt32 Estoque { get; set; }
+        public UInt32 EstoqueMinimo = 10;
+        public Boolean Ativo { get; set; }
+    }
+}
