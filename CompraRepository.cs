c9604f42ae76920fe3625a75be8db1a404efd6c1
guoliveira0 <luizgustavo160206@gmail.com>
2025-11-11 07:53:14 -0300

    CompraRepository

---
+++ CompraRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroDeUsuario
+{
+    public class CompraRepository
+    {
+        public static void SaveOrUpdate(Compra compra)
+        {
+            try
+            {
+                using (Repository dbContext = new())
+                {
+                    if (compra.Id == 0)
+                    {
+                        dbContext.Compras.Add
+                    }
+                }
+            }
+        }
+
+
+
+
+    }
+
+}
