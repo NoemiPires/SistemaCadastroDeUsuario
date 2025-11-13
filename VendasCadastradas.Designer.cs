10b9e9cb0e7f18d243a1196bd9af9d601f683c91
guoliveira0 <luizgustavo160206@gmail.com>
2025-11-13 10:41:51 -0300

    .

--- VendasCadastradas.Designer.cs
+++ VendasCadastradas.Designer.cs
@@ -29,29 +29,55 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            lstVendasCadastradas = new ListBox();
+            grpDadosVendasCdastradas = new GroupBox();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(grpDadosVendasCdastradas);
+            panel1.Controls.Add(lstVendasCadastradas);
             panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(326, 426);
+            panel1.Size = new Size(464, 265);
             panel1.TabIndex = 0;
+            // 
+            // lstVendasCadastradas
+            // 
+            lstVendasCadastradas.FormattingEnabled = true;
+            lstVendasCadastradas.ItemHeight = 15;
+            lstVendasCadastradas.Location = new Point(3, 3);
+            lstVendasCadastradas.Name = "lstVendasCadastradas";
+            lstVendasCadastradas.Size = new Size(258, 259);
+            lstVendasCadastradas.TabIndex = 0;
+            // 
+            // grpDadosVendasCdastradas
+            // 
+            grpDadosVendasCdastradas.Location = new Point(276, 3);
+            grpDadosVendasCdastradas.Name = "grpDadosVendasCdastradas";
+            grpDadosVendasCdastradas.Size = new Size(185, 259);
+            grpDadosVendasCdastradas.TabIndex = 1;
+            grpDadosVendasCdastradas.TabStop = false;
+            grpDadosVendasCdastradas.Text = "groupBox1";
             // 
             // VendasCadastradas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(350, 450);
+            ClientSize = new Size(488, 289);
             Controls.Add(panel1);
             Name = "VendasCadastradas";
             Text = "VendasCadastradas";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private ListBox lstVendasCadastradas;
+        private GroupBox grpDadosVendasCdastradas;
     }
 }
