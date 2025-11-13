38c33664f9d25c07bd3a6311fd5fb3459a17a4fd
guoliveira0 <luizgustavo160206@gmail.com>
2025-11-13 09:33:11 -0300

    Refactor using directives and format property declaration
    Removed unused `using` directives and improved code formatting
    by adjusting the `Credenciais` property declaration.

--- CadastrarProdutos.Designer.cs
+++ CadastrarProdutos.Designer.cs
@@ -29,83 +29,162 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
+            lblPreco = new Label();
+            lblEstoque = new Label();
+            lblCategoria = new Label();
+            lblNome = new Label();
+            lblId = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            comboBox1 = new ComboBox();
+            ckbAtivo = new CheckBox();
+            btnCadastrar = new Button();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            panel1.Controls.Add(label5);
-            panel1.Controls.Add(label4);
-            panel1.Controls.Add(label3);
-            panel1.Controls.Add(label2);
-            panel1.Controls.Add(label1);
+            panel1.Controls.Add(btnCadastrar);
+            panel1.Controls.Add(ckbAtivo);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(lblPreco);
+            panel1.Controls.Add(lblEstoque);
+            panel1.Controls.Add(lblCategoria);
+            panel1.Controls.Add(lblNome);
+            panel1.Controls.Add(lblId);
             panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(464, 417);
+            panel1.Size = new Size(416, 364);
             panel1.TabIndex = 0;
             // 
-            // label1
+            // lblPreco
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(18, 33);
-            label1.Name = "label1";
-            label1.Size = new Size(63, 25);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            lblPreco.AutoSize = true;
+            lblPreco.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPreco.Location = new Point(50, 146);
+            lblPreco.Name = "lblPreco";
+            lblPreco.Size = new Size(60, 25);
+            lblPreco.TabIndex = 4;
+            lblPreco.Text = "Preco";
             // 
-            // label2
+            // lblEstoque
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(18, 79);
-            label2.Name = "label2";
-            label2.Size = new Size(63, 25);
-            label2.TabIndex = 1;
-            label2.Text = "label2";
+            lblEstoque.AutoSize = true;
+            lblEstoque.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEstoque.Location = new Point(31, 205);
+            lblEstoque.Name = "lblEstoque";
+            lblEstoque.Size = new Size(79, 25);
+            lblEstoque.TabIndex = 3;
+            lblEstoque.Text = "Estoque";
             // 
-            // label3
+            // lblCategoria
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(201, 196);
-            label3.Name = "label3";
-            label3.Size = new Size(63, 25);
-            label3.TabIndex = 2;
-            label3.Text = "label3";
+            lblCategoria.AutoSize = true;
+            lblCategoria.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblCategoria.Location = new Point(16, 268);
+            lblCategoria.Name = "lblCategoria";
+            lblCategoria.Size = new Size(94, 25);
+            lblCategoria.TabIndex = 2;
+            lblCategoria.Text = "Categoria";
             // 
-            // label4
+            // lblNome
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(209, 204);
-            label4.Name = "label4";
-            label4.Size = new Size(63, 25);
-            label4.TabIndex = 3;
-            label4.Text = "label4";
+            lblNome.AutoSize = true;
+            lblNome.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNome.Location = new Point(47, 85);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(63, 25);
+            lblNome.TabIndex = 1;
+            lblNome.Text = "Nome";
             // 
-            // label5
+            // lblId
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(18, 138);
-            label5.Name = "label5";
-            label5.Size = new Size(63, 25);
-            label5.TabIndex = 4;
-            label5.Text = "label5";
+            lblId.AutoSize = true;
+            lblId.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblId.ForeColor = Color.Black;
+            lblId.Location = new Point(79, 22);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(28, 25);
+            lblId.TabIndex = 0;
+            lblId.Text = "Id";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(125, 19);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(270, 33);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(125, 143);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(270, 33);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(125, 82);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(270, 33);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(125, 202);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(270, 33);
+            textBox4.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(125, 265);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(270, 33);
+            comboBox1.TabIndex = 9;
+            // 
+            // ckbAtivo
+            // 
+            ckbAtivo.AutoSize = true;
+            ckbAtivo.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ckbAtivo.Location = new Point(125, 324);
+            ckbAtivo.Name = "ckbAtivo";
+            ckbAtivo.Size = new Size(74, 29);
+            ckbAtivo.TabIndex = 10;
+            ckbAtivo.Text = "Ativo";
+            ckbAtivo.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCadastrar.Location = new Point(303, 324);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.Size = new Size(92, 29);
+            btnCadastrar.TabIndex = 11;
+            btnCadastrar.Text = "Cadastrar";
+            btnCadastrar.UseVisualStyleBackColor = true;
             // 
             // CadastrarProdutos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(488, 441);
+            ClientSize = new Size(440, 388);
             Controls.Add(panel1);
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "CadastrarProdutos";
             Text = "CadastrarProdutos";
             panel1.ResumeLayout(false);
@@ -116,10 +195,17 @@
         #endregion
 
         private Panel panel1;
-        private Label label2;
-        private Label label1;
-        private Label label5;
-        private Label label4;
-        private Label label3;
+        private Label lblNome;
+        private Label lblId;
+        private Label lblPreco;
+        private Label lblEstoque;
+        private Label lblCategoria;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button btnCadastrar;
+        private CheckBox ckbAtivo;
+        private ComboBox comboBox1;
     }
 }
