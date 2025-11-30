2f99f6b37819f1bb75918e799c92f640e7c76660
NOTE LENOVO <NOTE LENOVO@DESKTOP-GO9015E>
2025-11-30 00:51:20 -0300

    .

--- CadastroProduto.Designer.cs
+++ CadastroProduto.Designer.cs
@@ -28,12 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CadastroProduto";
+            pnlCadastroProduto = new Panel();
+            txtEstoqueMinimo = new TextBox();
+            txtValor = new TextBox();
+            txtQuantidade = new TextBox();
+            txtDescricao = new TextBox();
+            btnAcao = new Button();
+            chkEstado = new CheckBox();
+            lblEstado = new Label();
+            btnCategoria = new Button();
+            grpCategoria = new GroupBox();
+            cboCategoria = new ComboBox();
+            lblEstoqueMinimo = new Label();
+            lblPrecoUnidade = new Label();
+            lblQuantidade = new Label();
+            lblDescricao = new Label();
+            lblAlertaSucesso = new Label();
+            lblAlertaProduto = new Label();
+            lblAlertaVazio = new Label();
+            lblAvisoEstoqueMinimo = new Label();
+            lblAlertaValor = new Label();
+            pnlCadastroProduto.SuspendLayout();
+            grpCategoria.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlCadastroProduto
+            // 
+            pnlCadastroProduto.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlCadastroProduto.Controls.Add(lblAlertaValor);
+            pnlCadastroProduto.Controls.Add(lblAvisoEstoqueMinimo);
+            pnlCadastroProduto.Controls.Add(lblAlertaVazio);
+            pnlCadastroProduto.Controls.Add(lblAlertaProduto);
+            pnlCadastroProduto.Controls.Add(lblAlertaSucesso);
+            pnlCadastroProduto.Controls.Add(txtEstoqueMinimo);
+            pnlCadastroProduto.Controls.Add(txtValor);
+            pnlCadastroProduto.Controls.Add(txtQuantidade);
+            pnlCadastroProduto.Controls.Add(txtDescricao);
+            pnlCadastroProduto.Controls.Add(btnAcao);
+            pnlCadastroProduto.Controls.Add(chkEstado);
+            pnlCadastroProduto.Controls.Add(lblEstado);
+            pnlCadastroProduto.Controls.Add(btnCategoria);
+            pnlCadastroProduto.Controls.Add(grpCategoria);
+            pnlCadastroProduto.Controls.Add(lblEstoqueMinimo);
+            pnlCadastroProduto.Controls.Add(lblPrecoUnidade);
+            pnlCadastroProduto.Controls.Add(lblQuantidade);
+            pnlCadastroProduto.Controls.Add(lblDescricao);
+            pnlCadastroProduto.Location = new Point(12, 12);
+            pnlCadastroProduto.Name = "pnlCadastroProduto";
+            pnlCadastroProduto.Size = new Size(328, 300);
+            pnlCadastroProduto.TabIndex = 0;
+            // 
+            // txtEstoqueMinimo
+            // 
+            txtEstoqueMinimo.Location = new Point(116, 114);
+            txtEstoqueMinimo.Name = "txtEstoqueMinimo";
+            txtEstoqueMinimo.Size = new Size(83, 23);
+            txtEstoqueMinimo.TabIndex = 12;
+            // 
+            // txtValor
+            // 
+            txtValor.Location = new Point(113, 79);
+            txtValor.Name = "txtValor";
+            txtValor.Size = new Size(86, 23);
+            txtValor.TabIndex = 11;
+            // 
+            // txtQuantidade
+            // 
+            txtQuantidade.Location = new Point(91, 43);
+            txtQuantidade.Name = "txtQuantidade";
+            txtQuantidade.Size = new Size(65, 23);
+            txtQuantidade.TabIndex = 10;
+            // 
+            // txtDescricao
+            // 
+            txtDescricao.Location = new Point(96, 7);
+            txtDescricao.Name = "txtDescricao";
+            txtDescricao.Size = new Size(206, 23);
+            txtDescricao.TabIndex = 9;
+            // 
+            // btnAcao
+            // 
+            btnAcao.BackColor = Color.FromArgb(128, 255, 128);
+            btnAcao.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAcao.Location = new Point(239, 260);
+            btnAcao.Name = "btnAcao";
+            btnAcao.Size = new Size(75, 23);
+            btnAcao.TabIndex = 8;
+            btnAcao.Text = "Salvar";
+            btnAcao.UseVisualStyleBackColor = false;
+            // 
+            // chkEstado
+            // 
+            chkEstado.AutoSize = true;
+            chkEstado.Location = new Point(30, 264);
+            chkEstado.Name = "chkEstado";
+            chkEstado.Size = new Size(54, 19);
+            chkEstado.TabIndex = 7;
+            chkEstado.Text = "Ativo";
+            chkEstado.UseVisualStyleBackColor = true;
+            // 
+            // lblEstado
+            // 
+            lblEstado.AutoSize = true;
+            lblEstado.Location = new Point(23, 246);
+            lblEstado.Name = "lblEstado";
+            lblEstado.Size = new Size(45, 15);
+            lblEstado.TabIndex = 6;
+            lblEstado.Text = "Estado:";
+            // 
+            // btnCategoria
+            // 
+            btnCategoria.BackColor = Color.FromArgb(128, 255, 128);
+            btnCategoria.Location = new Point(163, 156);
+            btnCategoria.Name = "btnCategoria";
+            btnCategoria.Size = new Size(139, 23);
+            btnCategoria.TabIndex = 5;
+            btnCategoria.Text = "Criar nova categoria";
+            btnCategoria.UseVisualStyleBackColor = false;
+            // 
+            // grpCategoria
+            // 
+            grpCategoria.Controls.Add(cboCategoria);
+            grpCategoria.Location = new Point(13, 174);
+            grpCategoria.Name = "grpCategoria";
+            grpCategoria.Size = new Size(273, 53);
+            grpCategoria.TabIndex = 4;
+            grpCategoria.TabStop = false;
+            grpCategoria.Text = "CATEGORIA:";
+            // 
+            // cboCategoria
+            // 
+            cboCategoria.FormattingEnabled = true;
+            cboCategoria.Location = new Point(17, 22);
+            cboCategoria.Name = "cboCategoria";
+            cboCategoria.Size = new Size(243, 23);
+            cboCategoria.TabIndex = 0;
+            // 
+            // lblEstoqueMinimo
+            // 
+            lblEstoqueMinimo.AutoSize = true;
+            lblEstoqueMinimo.Location = new Point(13, 122);
+            lblEstoqueMinimo.Name = "lblEstoqueMinimo";
+            lblEstoqueMinimo.Size = new Size(97, 15);
+            lblEstoqueMinimo.TabIndex = 3;
+            lblEstoqueMinimo.Text = "Estoque mínimo:";
+            // 
+            // lblPrecoUnidade
+            // 
+            lblPrecoUnidade.AutoSize = true;
+            lblPrecoUnidade.Location = new Point(13, 87);
+            lblPrecoUnidade.Name = "lblPrecoUnidade";
+            lblPrecoUnidade.Size = new Size(94, 15);
+            lblPrecoUnidade.TabIndex = 2;
+            lblPrecoUnidade.Text = "Preço (unidade):";
+            // 
+            // lblQuantidade
+            // 
+            lblQuantidade.AutoSize = true;
+            lblQuantidade.Location = new Point(13, 51);
+            lblQuantidade.Name = "lblQuantidade";
+            lblQuantidade.Size = new Size(72, 15);
+            lblQuantidade.TabIndex = 1;
+            lblQuantidade.Text = "Quantidade:";
+            // 
+            // lblDescricao
+            // 
+            lblDescricao.AutoSize = true;
+            lblDescricao.Location = new Point(13, 10);
+            lblDescricao.Name = "lblDescricao";
+            lblDescricao.Size = new Size(61, 15);
+            lblDescricao.TabIndex = 0;
+            lblDescricao.Text = "Descrição:";
+            // 
+            // lblAlertaSucesso
+            // 
+            lblAlertaSucesso.AutoSize = true;
+            lblAlertaSucesso.BackColor = Color.FromArgb(128, 255, 128);
+            lblAlertaSucesso.Location = new Point(113, 285);
+            lblAlertaSucesso.Name = "lblAlertaSucesso";
+            lblAlertaSucesso.Size = new Size(104, 15);
+            lblAlertaSucesso.TabIndex = 13;
+            lblAlertaSucesso.Text = "Cadastro efetuado";
+            // 
+            // lblAlertaProduto
+            // 
+            lblAlertaProduto.AutoSize = true;
+            lblAlertaProduto.BackColor = Color.FromArgb(255, 128, 255);
+            lblAlertaProduto.Font = new Font("Segoe UI", 6.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAlertaProduto.Location = new Point(231, 33);
+            lblAlertaProduto.Name = "lblAlertaProduto";
+            lblAlertaProduto.Size = new Size(83, 12);
+            lblAlertaProduto.TabIndex = 14;
+            lblAlertaProduto.Text = "Produto já existente";
+            // 
+            // lblAlertaVazio
+            // 
+            lblAlertaVazio.AutoSize = true;
+            lblAlertaVazio.BackColor = Color.FromArgb(255, 128, 255);
+            lblAlertaVazio.Font = new Font("Segoe UI", 6.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAlertaVazio.Location = new Point(13, 140);
+            lblAlertaVazio.Name = "lblAlertaVazio";
+            lblAlertaVazio.Size = new Size(194, 12);
+            lblAlertaVazio.TabIndex = 15;
+            lblAlertaVazio.Text = "Obrigatório o preenchimento dos campos acima";
+            // 
+            // lblAvisoEstoqueMinimo
+            // 
+            lblAvisoEstoqueMinimo.AutoSize = true;
+            lblAvisoEstoqueMinimo.BackColor = Color.FromArgb(255, 128, 255);
+            lblAvisoEstoqueMinimo.Font = new Font("Segoe UI", 6.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAvisoEstoqueMinimo.Location = new Point(215, 101);
+            lblAvisoEstoqueMinimo.Name = "lblAvisoEstoqueMinimo";
+            lblAvisoEstoqueMinimo.Size = new Size(110, 36);
+            lblAvisoEstoqueMinimo.TabIndex = 16;
+            lblAvisoEstoqueMinimo.Text = "Obrigatóriamente estoque\r\n mínimo deve ser menor\r\nque o Estoque";
+            // 
+            // lblAlertaValor
+            // 
+            lblAlertaValor.AutoSize = true;
+            lblAlertaValor.BackColor = Color.FromArgb(255, 128, 255);
+            lblAlertaValor.Font = new Font("Microsoft Sans Serif", 8.25F);
+            lblAlertaValor.Location = new Point(238, 68);
+            lblAlertaValor.Name = "lblAlertaValor";
+            lblAlertaValor.Size = new Size(92, 26);
+            lblAlertaValor.TabIndex = 17;
+            lblAlertaValor.Text = "Obrigatóriamente\r\npreço maior que 0";
+            // 
+            // CadastroProduto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(352, 324);
+            Controls.Add(pnlCadastroProduto);
+            Name = "CadastroProduto";
+            Text = "CadastroProduto";
+            pnlCadastroProduto.ResumeLayout(false);
+            pnlCadastroProduto.PerformLayout();
+            grpCategoria.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pnlCadastroProduto;
+        private Label lblDescricao;
+        private GroupBox grpCategoria;
+        private Label lblEstoqueMinimo;
+        private Label lblPrecoUnidade;
+        private Label lblQuantidade;
+        private Button btnCategoria;
+        private ComboBox cboCategoria;
+        private TextBox txtValor;
+        private TextBox txtQuantidade;
+        private TextBox txtDescricao;
+        private Button btnAcao;
+        private CheckBox chkEstado;
+        private Label lblEstado;
+        private TextBox txtEstoqueMinimo;
+        private Label lblAlertaVazio;
+        private Label lblAlertaProduto;
+        private Label lblAlertaSucesso;
+        private Label lblAlertaValor;
+        private Label lblAvisoEstoqueMinimo;
     }
 }
--- CadastroProduto.cs
+++ CadastroProduto.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,395 @@ namespace SistemaCadastroDeUsuario
 {
     public partial class CadastroProduto : Form
     {
-        public CadastroProduto()
+        private static CadastroProduto? _instanceAdd;
+        private static CadastroProduto? _instanceAtt;
+        private Boolean _isAdd = false;
+        private static Produto? _produtoAtualizar = null;
+
+        #region DataSource
+        private static BindingList<Categoria>? CategoriasSalvas;
+        public static void UpdateListCategoriasSalvos()
+        {
+            List<Categoria> lista = CategoriaRepository.FindAll();
+
+            // Se não tem nada na lista
+            if (CategoriasSalvas == null) CategoriasSalvas = new BindingList<Categoria>();
+
+            // Limpa a lista antiga
+            CategoriasSalvas.Clear();
+
+            // Adiciona o "Título"
+            CategoriasSalvas.Add(new Categoria() { Nome = "Selecionar categoria" });
+
+            // Adiciona os itens atualizados
+            foreach (Categoria u in lista) CategoriasSalvas.Add(u);
+        }
+        public static BindingList<Categoria> GetListCategoriasSalvos()
+        {
+            UpdateListCategoriasSalvos();
+
+            return CategoriasSalvas!;
+        }
+        private CadastroProduto(Boolean add)
         {
             InitializeComponent();
+            btnAcao.Text = add ? "Adicionar" : "Atualizar";
+            Text = add ? "Adicionar Produtos" : "Atualizar Produtos";
+
+            _isAdd = add;
+
+            cboCategoria.DataSource = GetListCategoriasSalvos();
+            cboCategoria.DisplayMember = "Nome";
+
+            if (add)
+            {
+                txtDescricao.Clear();
+                txtQuantidade.Clear();
+                txtValor.Text = "R$ 0,00";
+                txtEstoqueMinimo.Text = "10";
+                cboCategoria.SelectedIndex = 0;
+                chkEstado.Checked = true;
+            }
+            else
+            {
+                UInt16 _num = 0;
+
+                foreach (Categoria c in CategoriasSalvas!)
+                {
+                    if (c.Id == _produtoAtualizar!.Categoria!.Id) break;
+
+                    _num += 1;
+                }
+
+                txtDescricao.Text = _produtoAtualizar!.Nome;
+                txtQuantidade.Text = _produtoAtualizar.Estoque.ToString();
+                txtValor.Text = _produtoAtualizar.Preco.ToString("C2");
+                txtEstoqueMinimo.Text = _produtoAtualizar.EstoqueMinimo.ToString();
+                cboCategoria.SelectedIndex = (int)_num;
+                chkEstado.Checked = _produtoAtualizar.Ativo;
+            }
+            txtValor.TextChanged += txtValor_TextChanged!;
+            txtValor.KeyPress += txtValor_KeyPress!;
+        }
+
+        public static void Set(Produto produto)
+        {
+            _produtoAtualizar = produto;
+        }
+
+        public static CadastroProduto GetInstance(Boolean add)
+        {
+            if (add)
+            {
+                if (_instanceAdd == null || _instanceAdd.IsDisposed)
+                    _instanceAdd = new CadastroProduto(add);
+
+                return _instanceAdd;
+            }
+            else
+            {
+                if (_instanceAtt == null || _instanceAtt.IsDisposed)
+                    _instanceAtt = new CadastroProduto(add);
+
+                return _instanceAtt;
+            }
+        }
+
+        private void btnAcao_Click(object sender, EventArgs e)
+        {
+            // Deixando todos os alertas invisíveis
+            lblAlertaVazio.Visible = false;
+            lblAlertaSucesso.Visible = false;
+            lblAlertaProduto.Visible = false;
+            lblAvisoEstoqueMinimo.Visible = false;
+            lblAlertaValor.Visible = false;
+
+            // Verificando se as caixas de texto estão vazias
+            if (txtDescricao.Text.Trim() == "" || txtQuantidade.Text.Trim() == ""
+                || txtValor.Text.Trim() == "" || cboCategoria.SelectedIndex <= 0 || txtEstoqueMinimo.Text.Trim() == "")
+            {
+                lblAlertaVazio.Visible = true;
+                return;
+            }
+
+            // Se for pra adicionar checa se tem nome duplicado
+            if (_isAdd)
+            {
+                foreach (Produto pr in ProdutoRepository.FindAll())
+                {
+                    if (pr.Nome!.ToLower() == txtDescricao.Text.ToLower())
+                    {
+                        lblAlertaProduto.Visible = true;
+                        return;
+                    }
+                }
+            }
+
+            // Verificando se o estoque mínimo é maior que o estoque atual
+            if (txtEstoqueMinimo.Text.Trim() != "" && txtQuantidade.Text.Trim() != "")
+            {
+                UInt32 estoqueMinimo = UInt32.Parse(txtEstoqueMinimo.Text.Trim());
+                UInt32 estoqueAtual = UInt32.Parse(txtQuantidade.Text.Trim());
+                if (estoqueMinimo > estoqueAtual)
+                {
+                    lblAvisoEstoqueMinimo.Visible = true;
+                    return;
+                }
+            }
+
+            if (txtValor.Text.Trim() == "R$0,00")
+            {
+                lblAlertaValor.Visible = true;
+                return;
+            }
+
+            // Criando o objeto com os dados
+            Produto p = new Produto()
+            {
+                Id = _isAdd ? 0 : _produtoAtualizar!.Id,
+                Nome = txtDescricao.Text.Trim(),
+                Estoque = UInt32.Parse(txtQuantidade.Text.Trim()),
+                Preco = ParsePreco(txtValor.Text.Trim()),
+                Categoria = cboCategoria.SelectedItem as Categoria,
+                EstoqueMinimo = UInt32.Parse(txtEstoqueMinimo.Text.Trim()),
+                Ativo = chkEstado.Checked
+            };
+
+            // Salva no banco de dados
+            ProdutoRepository.SaveOrUpdate(p);
+
+            // Limpa as caixas de texto
+            txtDescricao.Clear();
+            txtQuantidade.Clear();
+            txtValor.Clear();
+            txtDescricao.Focus();
+            cboCategoria.SelectedIndex = 0;
+            chkEstado.Checked = true;
+            txtEstoqueMinimo.Text = "10";
+
+            lblAlertaSucesso.Visible = true;
+            ListaProdutos.GetInstance().AtualizarGrade();
+        }
+
+        private void btnCategoria_Click(object sender, EventArgs e)
+        {
+            CadastroCategoria janela = CadastroCategoria.GetInstance();
+            janela.ShowDialog(this);
+            janela.BringToFront();
+            janela.Focus();
+        }
+
+
+        private Boolean travarDescricao = false;
+        private const int descricaoMax = 40;
+        private void txtDescricao_TextChanged(object sender, EventArgs e)
+        {
+            lblAlertaVazio.Visible = false;
+            lblAlertaSucesso.Visible = false;
+            lblAlertaProduto.Visible = false;
+            lblAvisoEstoqueMinimo.Visible = false;
+            lblAlertaValor.Visible = false;
+
+            if (travarDescricao) return;
+
+            travarDescricao = true;
+
+            TextBox t = (TextBox)sender;
+
+            String texto = Regex.Replace(t.Text, @"[^ \p{L}\d]", "");
+
+            if (texto.Length > descricaoMax)
+            {
+                texto = texto.Substring(0, descricaoMax);
+            }
+
+            if (t.Text != texto)
+            {
+                t.Text = texto;
+                t.SelectionStart = t.Text.Length;
+            }
+
+            travarDescricao = false;
+        }
+
+        private void cboCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblAlertaVazio.Visible = false;
+            lblAlertaSucesso.Visible = false;
+            lblAlertaProduto.Visible = false;
+            lblAvisoEstoqueMinimo.Visible = false;
+            lblAlertaValor.Visible = false;
+        }
+
+        private decimal ParsePreco(String input)
+        {
+            if (String.IsNullOrWhiteSpace(input)) return 0m;
+
+            String s = input.Trim()
+                            .Replace("R$", "", StringComparison.OrdinalIgnoreCase)
+                            .Replace("$", "")
+                            .Replace(" ", "");
+
+            var pt = CultureInfo.GetCultureInfo("pt-BR");
+            if (Decimal.TryParse(s, System.Globalization.NumberStyles.Number | System.Globalization.NumberStyles.AllowCurrencySymbol, pt, out var valor))
+                return valor;
+
+            // Se falhar, devolve 0 (mantém comportamento simples). Você pode validar e mostrar erro ao usuário se preferir.
+            return 0m;
+        }
+
+        private Boolean travar = false;
+
+        private const int digitoMax = 8;
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+            lblAlertaVazio.Visible = false;
+            lblAlertaSucesso.Visible = false;
+            lblAlertaProduto.Visible = false;
+            lblAvisoEstoqueMinimo.Visible = false;
+            lblAlertaValor.Visible = false;
+
+            if (travar) return;
+
+            travar = true;
+
+            TextBox t = (TextBox)sender;
+
+            String numeros = System.Text.RegularExpressions.Regex.Replace(t.Text, @"[^\d]", "");
+
+            if (numeros.Length > digitoMax)
+            {
+                numeros = numeros.Substring(0, digitoMax);
+            }
+
+
+            if (numeros.Length == 0)
+            {
+                numeros = "0";
+            }
+
+            Decimal valor = Decimal.Parse(numeros) / 100m;
+
+            t.Text = valor.ToString("C2", new System.Globalization.CultureInfo("pt-BR"));
+
+            t.SelectionStart = t.Text.Length;
+
+            travar = false;
+
+        }
+
+        private void txtValor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        private Boolean travarQuantidade = false;
+
+        private void txtQuantidade_TextChanged(object sender, EventArgs e)
+        {
+            lblAlertaVazio.Visible = false;
+            lblAlertaSucesso.Visible = false;
+            lblAlertaProduto.Visible = false;
+            lblAvisoEstoqueMinimo.Visible = false;
+            lblAlertaValor.Visible = false;
+
+            if (travarQuantidade) return;
+
+            travarQuantidade = true;
+
+            TextBox t = (TextBox)sender;
+
+            String digitos = System.Text.RegularExpressions.Regex.Replace(t.Text, @"[^\d]", "");
+
+            if (digitos.Length == 0)
+            {
+                digitos = "";
+            }
+
+            if (digitos.Length > digitoMax)
+            {
+                digitos = digitos.Substring(0, digitoMax);
+            }
+
+            t.Text = digitos;
+            t.SelectionStart = t.Text.Length;
+
+            travarQuantidade = false;
+
+        }
+
+        private Boolean travarEstoqueMinimo = false;
+
+        private void txtEstoqueMinimo_TextChanged(object sender, EventArgs e)
+        {
+            lblAlertaVazio.Visible = false;
+            lblAlertaSucesso.Visible = false;
+            lblAlertaProduto.Visible = false;
+            lblAvisoEstoqueMinimo.Visible = false;
+            lblAlertaValor.Visible = false;
+
+            if (travarEstoqueMinimo) return;
+
+            travarEstoqueMinimo = true;
+
+            TextBox t = (TextBox)sender;
+
+            String digitos = System.Text.RegularExpressions.Regex.Replace(t.Text, @"[^\d]", "");
+
+            if (digitos.Length == 0)
+            {
+                digitos = "";
+            }
+
+            if (digitos.Length > digitoMax)
+            {
+                digitos = digitos.Substring(0, digitoMax);
+            }
+
+            t.Text = digitos;
+            t.SelectionStart = t.Text.Length;
+
+            travarEstoqueMinimo = false;
+
+        }
+
+        private void txtDescricao_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtQuantidade.Focus();
+                txtQuantidade.SelectAll();
+            }
+        }
+
+        private void txtQuantidade_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtValor.Focus();
+                txtValor.SelectAll();
+            }
+        }
+
+        private void txtEstoqueMinimo_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnAcao.Focus();
+            }
+        }
+
+        private void txtValor_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtEstoqueMinimo.Focus();
+                txtEstoqueMinimo.SelectAll();
+            }
         }
     }
 }
